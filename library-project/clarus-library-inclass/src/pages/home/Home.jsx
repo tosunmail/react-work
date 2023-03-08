c9b26d36128b1c392e0a9328b3f2df6de8052b82
tarik <tosunmaildir@gmail.com>
2023-03-08 18:20:10 +0100

    library app continious

---
+++ library-project/clarus-library-inclass/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HomeContainer } from './Home.style'
+
+const Home = () => {
+  return (
+    <HomeContainer>
+      
+    </HomeContainer>
+  )
+}
+
+export default Home
