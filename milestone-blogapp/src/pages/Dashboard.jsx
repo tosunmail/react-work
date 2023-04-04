ab3b0238134bd04c7ee2721111148c2c76dc8481
tosunmail <tosunmaildir@gmail.com>
2023-04-04 14:03:41 +0200

    dashboard

--- milestone-blogapp/src/pages/Dashboard.jsx
+++ milestone-blogapp/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "../components/Card";
+import { Grid } from "@mui/material";
 
 function CardComponent() {
   const [data, setData] = useState([]);
@@ -21,9 +22,15 @@ function CardComponent() {
 
   return (
     <div>
-      {data.map((item) => (
-        <Card key={item.id} item={item} />
+        <Grid container>
+               {data.map((item) => (
+    
+              <Card key={item.id} item={item} />
+        
+      
       ))}
+        </Grid>
+   
     </div>
   );
 }
