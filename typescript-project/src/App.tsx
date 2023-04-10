0e9c3a6ba1c1669a33755f4ad3a4a40a02344ef2
tosunmail <tosunmaildir@gmail.com>
2023-04-10 21:51:31 +0200

    typscript-react project start

---
+++ typescript-project/src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import FavoritesPage from "./pages/FavoritesPage";
+import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+function App() {
+  return (
+    <div className="bg-gray-400 min-h-screen ">
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="" element={<Home />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ typescript-project/src/features/productsSlice.ts
@@ -0,0 +1,70 @@
+
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+
+export interface Product {
+    id:                 number;
+    title:              string;
+    description:        string;
+    price:              number;
+    discountPercentage: number;
+    rating:             number;
+    stock:              number;
+    brand:              string;
+    category:           string;
+    thumbnail:          string;
+    images:             string[];
+}
+
+// Define a type for the slice state
+interface ProductsState {
+  loading:boolean,
+  error:boolean,
+  favorites:Product[] ,
+  productsList:Product[]
+}
+
+// Define the initial state using that type
+const initialState: ProductsState = {
+  loading:false ,
+  error:false,
+  favorites:[],
+  productsList:[]
+}
+
+export const productsSlice = createSlice({
+  name: 'products',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+fetchStart(state) {
+    state.loading = true;
+    state.error = false
+},
+getSuccessProduct (state,action:PayloadAction<Product[]>) {
+    state.loading = false ;
+    state.error = false;
+    state.productsList= action.payload
+},
+addFavorites (state , action:PayloadAction<Product>) {
+    state.favorites = [...state.favorites , action.payload] 
+} ,
+removeFavorites (state, action:PayloadAction<Product[]>) {
+    state.favorites = action.payload
+} ,
+fetchFail(state) {
+    state.loading = false;
+    state.error = true
+},
+
+
+
+    
+  },
+})
+
+export const { fetchFail , fetchStart, addFavorites,getSuccessProduct , removeFavorites} = productsSlice.actions
+
+
+export const productsReducer = productsSlice.reducer
